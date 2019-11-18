592781febd1e37925ab6662dd1d8f07be5f49f14
Kristian Jensen <rc0038618@student.riversidecollege.ac.uk>
2019-11-18 15:09:13 +0000

    i added a text box

--- Hello World/Form1.Designer.cs
+++ Hello World/Form1.Designer.cs
@@ -29,34 +29,45 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.txtBxName = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(460, 222);
+            this.button1.Location = new System.Drawing.Point(339, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(69, 26);
+            this.button1.Size = new System.Drawing.Size(92, 56);
             this.button1.TabIndex = 0;
             this.button1.Text = "Hello";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // txtBxName
+            // 
+            this.txtBxName.Location = new System.Drawing.Point(339, 74);
+            this.txtBxName.Name = "txtBxName";
+            this.txtBxName.Size = new System.Drawing.Size(92, 20);
+            this.txtBxName.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtBxName);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtBxName;
     }
 }
 
